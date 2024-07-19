177869db641b8076505956035c0d3a726fcbbf3c
Kazuki Baba <kzhrs4002@gmail.com>
2024-07-19 15:02:38 +0900

    no-cache_ok

--- routes/web.php
+++ routes/web.php
@@ -3,25 +3,6 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-// // 最初にアクセスした際はログインページにリダイレクト
-// Route::get('/', function () {
-//     return redirect()->route('login');
-// });
-
-// // ログインページ表示
-// Route::get('/login', [AuthController::class, 'showLoginForm'])->name('login');
-
-// // ログイン処理
-// Route::post('/login', [AuthController::class, 'login']);
-
-// // ホームページ（認証が必要）
-// Route::middleware('auth:sanctum')->get('/home', function () {
-//     return view('home');
-// })->name('home');
-
-// // ログアウト処理
-// Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-
 Route::middleware([
     'cache.headers:no_store;private;max_age=0',
 ])->group(function (): void {
